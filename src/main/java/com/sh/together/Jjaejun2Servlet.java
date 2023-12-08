c4ba774dc2a939f3715c9a62e4ba66091a2faca8
jjaejun <leejj3240@gmail.com>
2023-12-08 11:21:03 +0900

    Add: /jjaejun2 기능 수정
    - @WebServlet 수정

--- src/main/java/com/sh/together/Jjaejun2Servlet.java
+++ src/main/java/com/sh/together/Jjaejun2Servlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet
+@WebServlet("/jjaejun2")
 public class Jjaejun2Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
